7eed526e7d5015c7736a93614bba4d0629b8bab8
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2022-08-09 20:50:20 +0900

    Initial Commit

---
+++ todo-list-with-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-list-with-swift
+//
+//  Created by 吉岡雄也 on 2022/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo-list-with-swift/todo_list_with_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_list_with_swiftApp.swift
+//  todo-list-with-swift
+//
+//  Created by 吉岡雄也 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct todo_list_with_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
