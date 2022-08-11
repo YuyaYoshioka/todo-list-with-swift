080658030ebb38d6751242566fbbc8483717832b
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2022-08-12 01:27:08 +0900

    commit

--- todo-list-with-swift/TodoInput.swift
+++ todo-list-with-swift/TodoInput.swift
@@ -10,26 +10,80 @@ import SwiftUI
 struct TodoInput: View {
     @State private var todoInput: TodoInputEntity = TodoInputEntity(title: "", description: "", limitTime: Date())
     var addTodo: ((TodoInputEntity) -> Void)
+    var closeAddTodo: () -> Void
     
     var body: some View {
-        VStack {
-            TextField(
-                "Title",
-                text: $todoInput.title
-            )
-            .border(.primary)
-            Button(action: {
-                addTodo(todoInput)
-            }) {
-                Text("追加")
+        NavigationView {
+            VStack(spacing: 10) {
+                VStack(alignment: .leading) {
+                    Text("タイトル")
+                        .padding(.leading)
+                    TextField(
+                        "",
+                        text: $todoInput.title
+                    )
+                    .padding(4)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(.primary)
+                    }
+                    .padding([.leading, .trailing])
+                }
+                VStack(alignment: .leading) {
+                    Text("内容")
+                        .padding(.leading)
+                    TextEditor(text: $todoInput.description)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(.primary)
+                        }                    .padding([.leading, .trailing])
+                }
+                DatePicker(
+                    "期限",
+                    selection: $todoInput.limitTime,
+                    displayedComponents: [.date]
+                )
+                .environment(\.locale, Locale.init(identifier: "ja_JP"))
+                .padding([.leading, .trailing])
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        addTodo(todoInput)
+                        closeAddTodo()
+                    }) {
+                        Text("追加")
+                    }
+                    .disabled(isButtonDisabled(todo: todoInput))
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        closeAddTodo()
+                    }) {
+                        Text("キャンセル")
+                            .foregroundColor(.red)
+                    }
+                }
             }
         }
+    }
+    
+    private func isButtonDisabled(todo: TodoInputEntity) -> Bool {
+        if todo.title.isEmpty {
+            return true
+        }
+        if todo.description.isEmpty {
+            return true
+        }
+        
+        return false
     }
 }
 
 struct TodoInput_Previews: PreviewProvider {
     static var previews: some View {
-        TodoInput(addTodo: {_ in addTodo(todo: todoInput)})
+        TodoInput(addTodo: {_ in addTodo(todo: todoInput)}, closeAddTodo: {})
     }
     
     static func addTodo(todo: TodoInputEntity) -> Void {
--- todo-list-with-swift/TodoLists.swift
+++ todo-list-with-swift/TodoLists.swift
@@ -11,18 +11,40 @@ struct TodoLists: View {
     @State var todos: [TodoEntity]
     @State var todoID = 100
     @State var isAddMode = false
+    @State var showsDeleteConfirmation = false
     
     var body: some View {
         if isAddMode {
-            TodoInput(addTodo: addTodo)
+            TodoInput(addTodo: addTodo, closeAddTodo: closeAddTodo)
         } else {
             NavigationView {
-                ScrollView {
+                List {
                     ForEach(todos) { todo in
-                        NavigationLink(todo.title, destination: TodoDetail(todo: todo))
+                        NavigationLink(
+                            todo.title,
+                            destination: TodoDetail(todo: todo)
+                        )
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive) {
+                                showsDeleteConfirmation = true
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .alert("本当に削除しますか？",
+                                   isPresented: $showsDeleteConfirmation) {
+                                Button(action: {
+                                    deleteTodo(todoID: todo.id)
+                                }) {
+                                    Text("削除する")
+                                }
+                                Button(action: {}) {
+                                    Text("キャンセル")
+                                }
+                            }
+                        }
                     }
                 }
-                .navigationTitle("Todoリスト")
+//                .navigationTitle("Todoリスト")
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button(action: {
@@ -39,6 +61,15 @@ struct TodoLists: View {
     private func addTodo(todo: TodoInputEntity) {
         todos.insert(TodoEntity(id: todoID, title: todo.title, description: todo.description, isCompleted: false, limitTime: todo.limitTime), at: 0)
         todoID += 1
+    }
+    
+    private func closeAddTodo() {
+        isAddMode = false
+    }
+    
+    private func deleteTodo(todoID: Int) {
+        let targetIndex = todos.firstIndex(where: { $0.id == todoID })
+        todos.remove(at: targetIndex!)
     }
 }
 
